9c51f4c8dad218aa1608c36327a7ac89c74a76bb
radenle <43663248+radenle@users.noreply.github.com>
2018-10-29 22:44:41 -0500

    Add files via upload

---
+++ chap7 extra 1.py
@@ -0,0 +1,7 @@
+firstName = input("Enter the first name : ")
+
+middleName = input("Enter the middle name :")
+lastName = input("Enter the last name : ")
+
+print("disply the first , middle and last initials...")
+print(firstName[0],".",middleName[0],".",lastName[0],sep="")
