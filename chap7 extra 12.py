1fefb0cb0e6317dc9d7a45811e542b1347cb78fa
radenle <43663248+radenle@users.noreply.github.com>
2018-10-29 23:38:08 -0500

    Add files via upload

---
+++ chap7 extra 12.py
@@ -0,0 +1,12 @@
+getSentence = input("Enter the string : ")
+
+
+lists = getSentence.split()
+
+print("English :",getSentence)
+
+print("Pig Latin : ",end="")
+for i in lists:
+    newString = i[1:len(i)] +\
+                i[0]+"AY"
+    print(newString.upper(),end=" ")
